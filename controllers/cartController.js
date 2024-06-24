ddb9f4a8731a4501039c16c1526b5fa821356ab4
Ved-panchal <vedpanchal99@gmail.com>
2024-06-24 11:49:49 +0530

    final backend

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -99,3 +99,21 @@ export const updateCart = async (req, res) => {
     }
 };
 
+export const emptyCart = async (req, res) => {
+    const { userId } = req.params;
+
+    try {
+        let cart = await CartModel.findOne({ user_id: userId });
+        if (!cart) {
+            return res.status(404).json({ error: 'Cart not found' });
+        }
+
+        cart.products = [];
+        await cart.save();
+        res.status(200).json(cart);
+    } catch (err) {
+        console.error('Error emptying cart:', err);
+        res.status(500).json({ error: 'Server error' });
+    }
+};
+
--- routes/cartRoutes.js
+++ routes/cartRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { addToCart, getCart, removeFromCart, updateCart } from '../controllers/cartController.js';
+import { addToCart, emptyCart, getCart, removeFromCart, updateCart } from '../controllers/cartController.js';
 import authMiddleware from '../middlewares/authMiddlewares.js';
 
 const cartRouter = express.Router();
@@ -8,5 +8,6 @@ cartRouter.post('/cart/:userId', authMiddleware, addToCart);
 cartRouter.put('/cart/:userId', authMiddleware, updateCart);
 cartRouter.get('/cart/:userId', authMiddleware, getCart);
 cartRouter.delete('/cart/:userId', authMiddleware, removeFromCart);
+cartRouter.delete('/cart/:userId/empty',authMiddleware, emptyCart);
 
 export default cartRouter;
